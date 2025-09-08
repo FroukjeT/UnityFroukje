62e95fe08f22b83c16204b22040e646829c469d7
Cloudperry <roni.hokkanen@protonmail.com>
2025-09-08 14:59:11 +0300

    particles

--- Assets/CollisionDetector.cs
+++ Assets/CollisionDetector.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class CollisionDetector : MonoBehaviour
 {
+    public GameObject particles;
     void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
+        Instantiate(particles, transform.position, transform.rotation);
     }
 }
