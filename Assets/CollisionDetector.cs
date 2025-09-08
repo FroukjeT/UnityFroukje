2db4beb0b24c29c266359e57533d15c39aa7f255
Cloudperry <roni.hokkanen@protonmail.com>
2025-09-08 16:13:39 +0300

    final first thing

--- Assets/CollisionDetector.cs
+++ Assets/CollisionDetector.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 public class CollisionDetector : MonoBehaviour
 {
     public GameObject particles;
+    public AudioSource collisionSound;
     void OnTriggerEnter(Collider other)
     {
         Destroy(other.gameObject);
         Instantiate(particles, transform.position, transform.rotation);
+        collisionSound.Play();
     }
 }
